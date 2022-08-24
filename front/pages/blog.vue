e202993326eb7d3d104eabc7bb09d02572b94042
n.takahashi <n.takahashi@4qualia.co.jp>
2022-08-24 13:58:19 +0900

    blog画面実装

--- front/pages/blog.vue
+++ front/pages/blog.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="user">
+  <div>
     <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <p>自身が作成した記事の一部</p>
+      <img src="@/assets/image/blog.png" class="mb-5" />
+      <!-- <p>自身が作成した記事の一部</p> -->
     </v-col>
-
-    <div class="v-card" v-for="item in items" :key="item">
-      <v-card class="mx-auto" max-width="500">
+    <div class="v-card-comp" v-for="item in items" :key="item">
+      <v-card class="mx-auto" max-width="500" :href="item.link" target="_blank">
+        <v-img class="white--text align-end" height="11em" :src="item.src"></v-img>
         <v-card-text>
           <div></div>
           <p class="text-h4 text--primary">
@@ -14,16 +14,11 @@
           </p>
           
         </v-card-text>
-        <v-card-actions>
-          <a :href="item.link" target="_blank"
-            ><v-btn class="mx-2" fab dark large color="purple">
-              <v-icon dark> mdi-post-outline </v-icon>
-            </v-btn></a
-          ><br />
-        </v-card-actions>
       </v-card>
     </div>
   </div>
+
+  
 </template>
 
 <script>
@@ -33,34 +28,39 @@ export default {
     return {
       items: {
         blog: {
-          title: "BamsCatalog改修:GitHubのWebhookにも対応させる",
+          title: "GitHubのWebhookによりリポジトリをクローンする",
           link: "https://immediate-fruitadens-3bf.notion.site/BamsCatalog-GitHub-Webhook-4af1137b2ef8411b9aec9a501075591e",
+          src: require('@/assets/image/Notion.png')
         },
         blog2: {
-          title:
-            "BAMsCatalog改修:stoplight最新UIのソースを持ってきてカタログに取り込む",
+          title: "stoplight最新UIのソースを持ってきてアプリUIに取り込む",
           link: "https://immediate-fruitadens-3bf.notion.site/BAMsCatalog-stoplight-UI-d6215d060aab4383a5c8eb778a4f5ede",
+          src: require('@/assets/image/Notion.png')
         },
         blog3: {
-          title: "BAMsCatalog改修:JWA向け改修→添付資料のアップロード機能",
+          title: "画像,PDFファイルのアップロード,ダウンロード機能実装",
           link: "https://immediate-fruitadens-3bf.notion.site/BAMsCatalog-JWA-aaf8196759954afa99bd19d74cddd4f4",
+          src: require('@/assets/image/Notion.png')
         },
         blog4: {
-          title:
-            "各GitのWebhook連携を行う、GitLabとGitHubのnpmライブラリを使用して必要なプロジェクト情報を取得する",
+          title: "GitLab,GitHubのWebhook連携を行う",
           link: "https://docbase.io/posts/2363286/sharing/095f9146-a335-4345-8553-35f828e4f1b9",
+          src: require('@/assets/image/Docbase.png')
         },
         blog5: {
           title: "Stoplight Elementsを試してみる",
           link: "https://docbase.io/posts/2388612/sharing/bfd69dbc-70ce-48d7-a60d-c656d49d86a3",
+          src: require('@/assets/image/Docbase.png')
         },
         blog6: {
           title: "OpenAPI Specの基本とStoplight Studioの使い方",
           link: "https://docbase.io/posts/2539970/sharing/b9338129-576c-444d-bec6-932184232993",
+          src: require('@/assets/image/Docbase.png')
         },
         blog7: {
           title: "Node.jsにおけるファイル操作モジュール「fs」",
           link: "https://docbase.io/posts/2539976/sharing/bfa73534-4c7e-4f84-8153-218fe1b166ca",
+          src: require('@/assets/image/Docbase.png')
         },
       },
     };
@@ -69,12 +69,10 @@ export default {
 </script>
 
 <style scoped>
-.v-card {
+.v-card-comp {
   display: flex;
   justify-content: flex-end;
-  /* margin-bottom: 55px; */
-  margin: 15px 15px 15px 30px;
-  /* padding-bottom: 30px; */
+  margin: 1em 1em 1em 2em;
   float: left;
   align-items: center;
 }
--- front/pages/works.vue
+++ front/pages/works.vue
@@ -1,5 +1,11 @@
 <template>
+  <div>
+    <v-col class="text-center">
+      <img src="@/assets/image/file.png" class="mb-5" />
+      <!-- <p>自身が作成した記事の一部</p> -->
+    </v-col>
   <time-line></time-line>
+  </div>
 </template>
 
 <script>
